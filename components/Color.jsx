5bafdf2c010212c8c1b9428f33e81a4f21acb99a
jkimunyi-dev <jkkimunyi@gmail.com>
2024-07-03 21:08:57 +0300

    Update State flow ,Added onRemove and onRate

--- components/Color.jsx
+++ components/Color.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import StarRating from "./StarRating";
 import { FaTrashAlt } from "react-icons/fa";
 
-const Color = ({ id, title, color, rating, onRemove = (f) => f }) => {
-  console.log(rating);
+const Color = ({
+  id,
+  title,
+  color,
+  rating,
+  onRemove = (f) => f,
+  onRate = (f) => f,
+}) => {
   return (
     <section>
       <h1> {title} </h1>
@@ -11,7 +17,10 @@ const Color = ({ id, title, color, rating, onRemove = (f) => f }) => {
         <FaTrashAlt />
       </button>
       <div style={{ height: 50, backgroundColor: color }}>
-        <StarRating selectedStars={rating} />
+        <StarRating
+          selectedStars={rating}
+          onRate={(rating) => onRate(id, rating)}
+        />
       </div>
     </section>
   );
