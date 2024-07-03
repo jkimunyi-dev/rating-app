af77684f7bed2e613fad0c4fe854737ebd3b4b30
jkimunyi-dev <jkkimunyi@gmail.com>
2024-07-03 20:40:10 +0300

    Fixed Bug

--- components/StarRating.jsx
+++ components/StarRating.jsx
@@ -5,16 +5,21 @@ import { useState } from "react";
 
 const createArray = (length) => [...Array(length)];
 
-const StarRating = ({ style = {}, totalStars = 5, ...props }) => {
-  const [selectedStars, setSelectedStars] = useState(0);
+const StarRating = ({
+  style = {},
+  totalStars = 5,
+  selectedStars,
+  ...props
+}) => {
+  const [currentStars, setCurrentStars] = useState(selectedStars || 0);
 
   return (
     <div style={{ padding: "10px", ...style }} {...props}>
       {createArray(totalStars).map((n, i) => (
         <Star
           key={i}
-          selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
+          selected={currentStars > i}
+          onSelect={() => setCurrentStars(i + 1)}
         />
       ))}
       <p>
