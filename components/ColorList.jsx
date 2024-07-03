76ed65daddd89ca345b38dc9b9cbfda03f54397e
jkimunyi-dev <jkkimunyi@gmail.com>
2024-07-03 20:28:31 +0300

    Structure updates

--- components/ColorList.jsx
+++ components/ColorList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Color from "./Color";
 
 const ColorList = ({ colors = [] }) => {
-  if (!colors.length) return <div> No colors listed </div>;
+  if (!colors.length) return <div> No colors listed. (Add a color) </div>;
   return (
     <div>
       {colors.map((color) => (
