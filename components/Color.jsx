dd9551980e0b852b79d72bc519790e618c3b2ddc
jkimunyi-dev <jkkimunyi@gmail.com>
2024-07-03 19:49:07 +0300

    Separate Color entity

---
+++ components/Color.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import StarRating from "./StarRating";
+
+const Color = ({ title, color, rating }) => {
+  return (
+    <section>
+      <h1> {title} </h1>
+      <div style={{ height: 50, backgroundColor: color }}>
+        <StarRating selectedStars={rating} />
+      </div>
+    </section>
+  );
+};
+
+export default Color;
