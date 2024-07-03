5f7ddbde2251973ae76eafb7bfef98324a710558
jkimunyi-dev <jkkimunyi@gmail.com>
2024-07-03 20:28:15 +0300

    Updates to color component

--- components/Color.jsx
+++ components/Color.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import StarRating from "./StarRating";
+import { FaTrashAlt } from "react-icons/fa";
 
-const Color = ({ title, color, rating }) => {
+const Color = ({ id, title, color, rating, onRemove = (f) => f }) => {
+  console.log(rating);
   return (
     <section>
       <h1> {title} </h1>
+      <button onClick={() => onRemove(id)}>
+        <FaTrashAlt />
+      </button>
       <div style={{ height: 50, backgroundColor: color }}>
         <StarRating selectedStars={rating} />
       </div>
