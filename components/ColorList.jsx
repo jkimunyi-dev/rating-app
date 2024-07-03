7953f10ea9ed5024e500c5fc2af04115769f9b45
jkimunyi-dev <jkkimunyi@gmail.com>
2024-07-03 19:48:53 +0300

    ColorLis Component

--- components/ColorList.jsx
+++ components/ColorList.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Color from "./Color";
 
-const ColorList = ({ colors }) => {
-  return <div>ColorList</div>;
+const ColorList = ({ colors = [] }) => {
+  if (!colors.length) return <div> No colors listed </div>;
+  return (
+    <div>
+      {colors.map((color) => (
+        <Color key={color.id} {...color} />
+      ))}
+    </div>
+  );
 };
 
 export default ColorList;
